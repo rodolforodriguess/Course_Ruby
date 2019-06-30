0b0b9bd0a087a28313be52a581f6d854b257df1e
Rodolfo Rodrigues <rodolfo@sk8.com.br>
2019-06-29 23:25:10 -0300

    Object oriented lessons

--- ClassesI/gadget.rb
+++ ClassesI/gadget.rb
@@ -1,12 +1,24 @@
 class Gadget
+  def initialize
+    @username = "User #{rand(1..100)}"
+    @password = "topsecret"
+    @production_number = "#{("a".."z").to_a.sample}-#{rand(1..999)}"
+  end
 
+  # def info
+  #   "Gadget #{@production_number} has the username #{@username}"
+  # end
+
+  def to_s
+    "Gadget #{@production_number} has the username #{@username}.
+     It is made from the #{self.class} class and it has the ID#{self.object_id}."
+  end
 end
 
-shiny = Gadget.new
-flashy = Gadget.new
+phone = Gadget.new
+# puts phone.info
+#puts phone.methods
+puts phone.to_s
 
-puts shiny
-puts flashy
-
-p shiny.object_id
-p flashy.object_id
+# laptop = Gadget.new
+# p laptop.instance_variables

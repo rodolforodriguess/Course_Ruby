a117ee15d05b68844464d23e01ebbf00f4238ae1
Rodolfo Rodrigues <rodolfo@sk8.com.br>
2019-07-10 23:12:46 -0300

    monkey patching ruby built-in classes and strucs

--- ClassesII/gadgetII.rb
+++ ClassesII/gadgetII.rb
@@ -1,13 +1,15 @@
+require_relative "app_store"
+
 class Gadget
 
   attr_accessor :username
-  attr_reader :production_number
-  attr_writer :password
+  attr_reader :production_number, :apps
 
   def initialize(username, password)
     @username = username#"User #{rand(1..100)}"
     @password = password#"topsecret"
     @production_number = generate_production_number
+    @apps = []
   end
 
   def to_s
@@ -15,7 +17,29 @@ class Gadget
      It is made from the #{self.class} class and it has the ID#{self.object_id}."
   end
 
+  def install_app(name)
+    app = AppStore.find_app(name)
+    @apps << app unless @apps.include?(app)
+  end
+
+  def delete_app(name)
+    app = apps.find { |installed_app| installed_app.name == name }
+    apps.delete(app) unless app.nil?
+  end
+
+  def reset(username, password)
+    self.username = username
+    self.password = password
+    self.apps = []
+  end
+
+  def password=(new_password)
+    @password = new_password if validate_password(new_password)
+  end
+
   private
+  attr_writer :apps
+
   def generate_production_number
     start_digits = rand(10000..99999)
     end_digits = rand(10000..99999)
@@ -24,9 +48,25 @@ class Gadget
     5.times {middle_digits << alphabet.sample}
     "#{start_digits}-#{middle_digits}-#{end_digits}"
   end
+
+  def validate_password(new_password)
+    new_password.is_a?(String) && new_password.length >= 6 && new_password =~ /\d/
+  end
+
 end
 
-phone = Gadget.new("user", "password")
-p phone.production_number
-p phone.to_s
-p phone.generate_production_number
+g = Gadget.new("rodolfo", "password")
+p g.apps
+
+g.install_app(:Chat)
+g.install_app(:Twitter)
+g.install_app(:Chat)
+p g.apps
+
+g.delete_app(:Chat)
+g.delete_app(:Twitter)
+p g.apps
+# phone = Gadget.new("rubyfan123", "programming123")
+# p phone.production_number
+# p phone.to_s
+#p phone.generate_production_number
